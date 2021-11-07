4bec12949b84a749efe5286f6fcffb65d2929694
saharoori <saharlaor@gmail.com>
2021-11-07 13:21:53 +0200

    JS functions basics

---
+++ JS/2-Functions/1-functions.js
@@ -0,0 +1,9 @@
+function decribeCountry(country, population, capitalCity) {
+    return `${country} has ${population} people and its capital city is ${capitalCity}`;
+}
+
+decribeCountry("Israel", 9217000, "Jerusalem");
+decribeCountry("Vietnam", 98517000, "Hanoi");
+decribeCountry("Tonga", 107345, "Nuku'alofa");
+// A bonus because it was funny
+decribeCountry("Holy See", 804, "Vatican City");
