2a252f02089d8e3cc29d99769f831cc215f46b39
saharoori <saharlaor@gmail.com>
2021-11-07 10:51:55 +0200

    Primitive JS types

---
+++ JS/1-Data-Types/1-primitive_data_types.js
@@ -0,0 +1,10 @@
+var str = "String";
+var n = 8;
+var obj = {
+    "key": "value",
+    "anotherkey": "anothervalue"
+};
+var isBool = true;
+var nothing = null;
+var something;
+var uniq = Symbol("One of a kind");
