9ff61eda59c75acaaf92a44e669aea2c7006046c
saharoori <saharlaor@gmail.com>
2021-11-09 09:35:02 +0200

    Number of siblings

---
+++ JS/4-Operations/1-number_of_siblings.js
@@ -0,0 +1,9 @@
+let numSiblings = Number(prompt("How many siblings do you have?"));
+
+if (numSiblings === 1) {
+    console.log("1 sibling!");
+} else if (numSiblings > 1) {
+    console.log("More tahn 1 sibling");
+} else {
+    console.log("No siblings");
+}
