21ee00026298c8ec8bba4eb7fc050b162c262002
saharoori <saharlaor@gmail.com>
2021-11-14 10:57:52 +0200

    Redo steps with whitespaces and no repeat

--- JS/9-Loop/4-steps.js
+++ JS/9-Loop/4-steps.js
@@ -1,6 +1,13 @@
 const steps = (num) => {
     for (let i = 1; i <= num; i++) {
-        console.log("#".repeat(i));
+        let step = "";
+        for (let j = 1; j <= i; j++) {
+            step += "#";
+        }
+        for (let j = i + 1; j <= num; j++) {
+            step += " ";
+        }
+        console.log(`'${step}'`);
     }
 };
 
