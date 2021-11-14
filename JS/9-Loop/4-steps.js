e8f105e35167c6d22a286273cd8f5f016454b16a
saharoori <saharlaor@gmail.com>
2021-11-14 11:53:39 +0200

    Steps in a single loop

--- JS/9-Loop/4-steps.js
+++ JS/9-Loop/4-steps.js
@@ -1,14 +1,19 @@
 const steps = (num) => {
-    for (let i = 1; i <= num; i++) {
-        let step = "";
-        for (let j = 1; j <= i; j++) {
-            step += "#";
-        }
-        for (let j = i + 1; j <= num; j++) {
-            step += " ";
-        }
-        console.log(`'${step}'`);
+    // for (let i = 1; i <= num; i++) {
+    //     let step = "";
+    //     for (let j = 1; j <= i; j++) {
+    //         step += "#";
+    //     }
+    //     for (let j = i + 1; j <= num; j++) {
+    //         step += " ";
+    //     }
+    //     console.log(`'${step}'`);
+    // }
+    let step = new Array(num).fill(" ");
+    for (let i = 0; step.indexOf(" ") !== -1; i++) {
+        step[step.indexOf(" ")] = "#";
+        console.log(`'${step.join("")}'`);
     }
 };
 
-steps(44);
+steps(4);
