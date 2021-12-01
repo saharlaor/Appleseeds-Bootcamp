3335dca648733b3f23759b62c1a947e94d044d22
saharoori <saharlaor@gmail.com>
2021-12-01 21:02:32 +0200

    Start without movement

--- Projects/Snake/dist/scripts/input.js
+++ Projects/Snake/dist/scripts/input.js
@@ -4,13 +4,14 @@ const DIRECTIONS = {
     ArrowRight: { x: 1, y: 0 },
     ArrowLeft: { x: -1, y: 0 },
 };
-let inputDirection = DIRECTIONS["ArrowRight"];
+let inputDirection = { x: 0, y: 0 };
 
 export function changeDirection(e) {
     const tempDirection = DIRECTIONS[e.key];
 
     // If the arrow changes the direction to the other make change
-    Math.abs(inputDirection.x) - Math.abs(tempDirection.x) &&
+    (Math.abs(inputDirection.x) - Math.abs(tempDirection.x) ||
+        Math.abs(inputDirection.y) - Math.abs(tempDirection.y)) &&
         (inputDirection = DIRECTIONS[e.key]);
 
     window.removeEventListener("keydown", changeDirection);
