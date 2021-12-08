a9d40f06f7f90acd60e9f5afc090bd369ec54bf1
saharoori <saharlaor@gmail.com>
2021-12-08 14:44:44 +0200

    30.1 - UX improvements

--- JS/30-More-APIs/1-github_api/script.js
+++ JS/30-More-APIs/1-github_api/script.js
@@ -5,18 +5,26 @@ const users = [];
 const usersContainerEl = document.querySelector(".users__container");
 const [searchTxt, searchBtn] =
   document.querySelector(".search__container").children;
+const errorEl = document.querySelector(".error__container");
 
 const getUserData = async (username) => {
   const reqUrl = `${GITHUB_URL}${username}`;
-  const req = await fetch(reqUrl);
-  const data = await req.json();
-  return {
-    username: username,
-    displayName: data.name,
-    image: data.avatar_url,
-    url: data.html_url,
-    publicRepos: data.public_repos,
-  };
+  try {
+    const req = await fetch(reqUrl);
+    if (req.status === 404) {
+      throw new Error(`User ${username} Not found`);
+    }
+    const data = await req.json();
+    return {
+      username: username,
+      displayName: data.name,
+      image: data.avatar_url,
+      url: data.html_url,
+      publicRepos: data.public_repos,
+    };
+  } catch (err) {
+    return err;
+  }
 };
 
 const getUserCard = (data) => {
@@ -39,14 +47,33 @@ const getUserCard = (data) => {
 };
 
 const searchUser = async (_) => {
+  errorEl.textContent = "";
   const username = searchTxt.value;
-  const userData = await getUserData(username);
-  const userCard = getUserCard(userData);
-  usersContainerEl.append(userCard);
+  searchTxt.value = "";
+  if (users.includes(username)) {
+    return;
+  } else {
+    users.push(username);
+  }
+  try {
+    const userData = await getUserData(username);
+    if (userData instanceof Error) {
+      throw userData;
+    }
+    const userCard = getUserCard(userData);
+    usersContainerEl.append(userCard);
+  } catch (err) {
+    errorEl.textContent = err;
+  }
+  searchTxt.focus();
 };
 
 function main() {
   searchBtn.addEventListener("click", searchUser);
+  searchTxt.addEventListener(
+    "keydown",
+    (e) => e.key === "Enter" && searchUser()
+  );
 }
 
 main();
