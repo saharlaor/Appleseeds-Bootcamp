a53c8f7a8d8f9909b6e00bbfa87aedc057c0da57
saharoori <saharlaor@gmail.com>
2022-01-09 17:12:59 +0200

    3.1 - Imports exports

---
+++ Node/3-Importing_Exporting/1-import_export.mjs
@@ -0,0 +1,16 @@
+// Import is the new way in ES6 to get an external module in your code
+// Require is the way to do it in NodeJS
+
+// Importing babel to your js module file lets you import it using the import keyword
+
+// __filename or __dirname are CommonJS variables, and are not present in ES6 modules.
+
+export const func1 = function () {
+  console.log(1);
+};
+export const func2 = function () {
+  console.log(2);
+};
+export const func3 = function () {
+  console.log(3);
+};
---
+++ Node/3-Importing_Exporting/test.mjs
@@ -0,0 +1,5 @@
+import { func1, func2, func3 } from "./1-import_export.mjs";
+
+func1();
+func2();
+func3();
