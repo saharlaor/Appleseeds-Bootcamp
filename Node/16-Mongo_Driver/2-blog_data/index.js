d3653ee6f0b40a80be5b51a6e2cc53160fe7b57b
saharoori <saharlaor@gmail.com>
2022-01-18 14:44:59 +0200

    16.2 - Blog data, insert new users with unique name

--- Node/16-Mongo_Driver/2-blog_data/index.js
+++ Node/16-Mongo_Driver/2-blog_data/index.js
@@ -4,6 +4,24 @@ const uri = "mongodb://127.0.0.1:27017";
 
 const client = new MongoClient(uri);
 
+async function addUsers(nameArr) {
+  const collection = client.db("blog").collection("users");
+
+  // Set a unique name index
+  await collection.createIndex({ name: 1 }, { unique: true });
+  try {
+    // Wait for all inserts to finish one way or another
+    await Promise.allSettled(
+      nameArr.map((name) => {
+        const user = { name, posts: [] };
+        return collection.insertOne(user);
+      }, [])
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 async function main() {
   try {
     // Connect the client to the server
@@ -12,6 +30,7 @@ async function main() {
     await client.db("admin").command({ ping: 1 });
     console.log("Connected successfully to server");
 
+    await addUsers(["Pini Hodadad", "Timmmmmmmmy"]);
   } catch (err) {
     console.log(
       `There was a problem with connecting to the MongoDB server\n\n${err}`
